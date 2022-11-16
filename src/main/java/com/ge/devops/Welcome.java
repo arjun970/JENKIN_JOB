c2c83732312870fe8e7d88c6bc57ae8064bb65f4
arjun970 <212529543@ge.com>
2022-11-16 11:15:36 +0530

    first commit

---
+++ src/main/java/com/ge/devops/Welcome.java
@@ -0,0 +1,25 @@
+package com.ge.devops;
+
+
+
+/**
+
+ * Hello world!
+
+ *
+
+ */
+
+public class Welcome
+
+{
+
+    public static void main( String[] args )
+
+    {
+
+        System.out.println( "Welcome World!" );
+
+    }
+
+}
